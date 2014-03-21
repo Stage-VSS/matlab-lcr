634dee35b971528967c74a32f2849e852b376ebd
Mark Cafaro <markcafaro@d-128-95-10-211.dhcp4.washington.edu>
2014-03-21 13:32:31 -0700

    Added setLedEnables

---
+++ lcrSetLedEnables.cpp
@@ -0,0 +1,22 @@
+#include "lcr.h"
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+    if (nrhs < 4 || nrhs > 4)
+    {
+        mexErrMsgIdAndTxt("lcr:usage", "Usage: lcrSetLedEnables(seqCtrl, red, green, blue)");
+        return;
+    }
+    
+    bool seqCtrl = mxGetScalar(prhs[0]);
+    bool red = mxGetScalar(prhs[1]);
+    bool green = mxGetScalar(prhs[2]);
+    bool blue = mxGetScalar(prhs[3]);
+    
+    int result = LCR_SetLedEnables(seqCtrl, red, green, blue);
+    if (result == -1)
+    {
+        mexErrMsgIdAndTxt("lcr:failedToSetLedEnables", "Failed to set LED enables");
+        return;
+    }
+}
